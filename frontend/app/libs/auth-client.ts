ff7c7db838e7feb011a3f149de98c2a2a89f3e2e
Code-ga <tritranduc11@gmail.com>
2025-01-21 00:31:53 +0700

    update api url

--- frontend/app/libs/auth-client.ts
+++ frontend/app/libs/auth-client.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000" // the base url of your auth server
+  baseURL: import.meta.env.DEV ? "http://localhost:3000" : "https://self-hosted-forum.nbth.hackclub.app" // the base url of your auth server
 })
--- frontend/app/libs/client.ts
+++ frontend/app/libs/client.ts
@@ -1,4 +1,4 @@
 import { treaty } from '@elysiajs/eden'
 import type { App } from '@/index'
 
-export const client = treaty<App>('localhost:3000') 
+export const client = treaty<App>(import.meta.env.DEV ? 'localhost:3000' : 'self-hosted-forum.nbth.hackclub.app') 
