b28cb9a9cfc9c9814c2d4fa81564618ddbc4d6d1
Code-ga <tritranduc11@gmail.com>
2025-01-23 01:20:00 +0700

    idk

--- frontend/vercel/prepare.ts
+++ frontend/vercel/prepare.ts
@@ -1,8 +1,12 @@
 import * as fsp from "node:fs/promises";
+import * as fs from "node:fs";
 
-await fsp.rm(".vercel", { recursive: true }).catch(() => {});
+await fsp.rm(".vercel", { recursive: true }).catch(() => { });
 await fsp.mkdir(".vercel/output/static", { recursive: true });
 
+if (fs.statSync(".vercel/output").isDirectory()) {
+  await fsp.rm(".vercel/output", { recursive: true });
+}
 await fsp.cp("vercel/output/", ".vercel/output", { recursive: true });
 await fsp.cp("build/client/", ".vercel/output/static", { recursive: true });
 await fsp.cp("build/server/", ".vercel/output/functions/index.func", {
